22912dca5ceba8d636e4564230d20e688f05d250
viktar <v-a-i@mail.ru>
2022-11-12 13:16:20 +0100

    Initial commit

---
+++ src/HomeWorkLevelTwo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HomeWorkLevelTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOne = scanner.nextInt();
+        int numberTwo = scanner.nextInt();
+        if (numberOne == 36 && numberTwo == 17) {
+            System.out.println("Джекпот");
+        } else if (numberOne == 36 || numberTwo == 17) {
+            System.out.println("Маленький выигрыш");
+        } else {
+            System.out.println("Все проиграл");
+        }
+    }
+}
